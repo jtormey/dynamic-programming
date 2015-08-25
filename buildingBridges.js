411773d7ec6f5971fcfd25b7021d1914f310ac0d
Justin Tormey <jrtormey@gmail.com>
2015-08-25 19:22:31 -0400

    Bridges solution partially working

--- buildingBridges.js
+++ buildingBridges.js
@@ -1,11 +1,34 @@
 
-var leftBank =  [1, 2, 3, 4, 5, 6];
-var rightBank = [4, 3, 5, 2, 1, 6];
+var rightBank = [1, 2, 3];
+var leftBank =  [3, 2, 1];
 
-function pairCities() {
-  // Your code here...
-  // Find the maximum number of bridges you
-  // can build without any crossing
+var pairs = rightBank.map(function(item, idx) {
+  return [item, leftBank[idx]];
+});
+
+function pairCities(pairs) {
+
+  var n = pairs.length;
+  var max = 0;
+  var dp = new Array(n);
+
+  for (var i = 0; i < dp.length; i++) {
+    dp[i] = 1;
+  }
+
+  for (var i = 1; i < n; i++) {
+    for (var j = 0; j < i; j++) {
+      if (pairs[i][1] > pairs[j][0]) {
+        dp[i] = Math.max(dp[i], dp[j] + 1);
+      }
+    }
+    max = Math.max(max, dp[i]);
+  }
+
+  return max;
 
 }
+
+var result = pairCities(pairs);
+console.log(result);
 

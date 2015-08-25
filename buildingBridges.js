dd684ece50853f512ba9020c91a1e86251ff0de7
Justin Tormey <jrtormey@gmail.com>
2015-08-25 18:57:33 -0400

    Initial commit, include problems

---
+++ buildingBridges.js
@@ -0,0 +1,11 @@
+
+var leftBank =  [1, 2, 3, 4, 5, 6];
+var rightBank = [4, 3, 5, 2, 1, 6];
+
+function pairCities() {
+  // Your code here...
+  // Find the maximum number of bridges you
+  // can build without any crossing
+
+}
+
---
+++ knapsack.js
@@ -0,0 +1,11 @@
+
+var weights = [2, 3, 4, 5];
+var values =  [3, 7, 2, 9];
+
+function knapsack(maxWeight, weights, values) {
+  // Your code goes here...
+  // Find the largest value you can store in
+  // the knapsack without exceeding maxWeight
+
+}
+

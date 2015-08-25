74eb46a9f31f89cb393d94136f67fa4989f18a29
Justin Tormey <jrtormey@gmail.com>
2015-08-25 19:00:52 -0400

    Solution for knapsack problem

--- knapsack.js
+++ knapsack.js
@@ -3,9 +3,33 @@ var weights = [2, 3, 4, 5];
 var values =  [3, 7, 2, 9];
 
 function knapsack(maxWeight, weights, values) {
-  // Your code goes here...
-  // Find the largest value you can store in
-  // the knapsack without exceeding maxWeight
 
+  var numItems = values.length;
+
+  var L = new Array(100);
+  for (var i = 0; i < 100; i++) {
+      L[i] = new Array(100);
+  }
+
+  for (var i = 0; i <= numItems; i++) {
+
+    for (var w = 0; w <= maxWeight; w++) {
+
+      if (i === 0 || w === 0) {
+        L[i][w] = 0;
+      } else if (weights[i-1] <= w) {
+        L[i][w] = Math.max(values[i-1] + L[i-1][w-weights[i-1]], L[i-1][w]);
+      } else {
+        L[i][w] = L[i-1][w];
+      }
+
+    }
+
+  }
+
+  return L[numItems][maxWeight];
 }
+
+var r = knapsack(5, weights, values);
+console.log(r);
 

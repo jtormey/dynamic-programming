2a5b32b60b2831532dbde3bbe2cad1cecaab6c89
Justin Tormey <jrtormey@gmail.com>
2015-08-26 21:16:00 -0400

    Expand on building bridges solution

--- buildingBridges.js
+++ buildingBridges.js
@@ -1,10 +1,13 @@
 
-var rightBank = [1, 2, 3];
-var leftBank =  [3, 2, 1];
+var rightBank = [2, 4, 1, 3];
+var leftBank =  [1, 2, 3, 4];
 
 var pairs = rightBank.map(function(item, idx) {
   return [item, leftBank[idx]];
 });
+
+// Stack overflow solution
+// note: doesn't seem to work
 
 function pairCities(pairs) {
 
@@ -29,6 +32,57 @@ function pairCities(pairs) {
 
 }
 
-var result = pairCities(pairs);
-console.log(result);
+console.log(pairCities(pairs));
+
+// My solution
+
+function findOpposites(cities, bank) {
+  var opposites = [];
+  for (var i = 0; i < cities.length; i++) {
+    var opposite = findOpposite(cities[i], bank);
+    opposites.push(opposite);
+  }
+  return opposites;
+}
+
+
+function findOpposite(city, bank) {
+  return bank.indexOf(city);
+}
+
+function pairThem(cities0, cities1) {
+
+  var opposites = findOpposites(cities0, cities1);
+  var currentCount = 1;
+  var maxCount = 1;
+
+  for (var i = 0; i < opposites.length; i++) {
+
+    if (opposites[i] > opposites[i - 1]) {
+      currentCount += 1;
+    } else {
+      maxCount = currentCount;
+      currentCount = 1;
+    }
+  }
+
+  return maxCount;
+
+}
+
+var res = pairThem(rightBank, leftBank, rightBank);
+console.log(res);
+
+
+
+
+
+
+
+
+
+
+
+
+
 
